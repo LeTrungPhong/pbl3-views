6972beeab7bfc4df18d34df72150f81d85b98a08
Trung Nguyễn <trung9204@gmail.com>
2024-04-25 22:45:13 +0700

    list rooms

--- assets/js/rooms.js
+++ assets/js/rooms.js
@@ -1,5 +1,6 @@
 const userOptions = document.getElementById("user_options")
 const QuantityPerson = document.getElementsByClassName("QuantityPerson")[0]
+const optionItem = document.getElementsByClassName("option__item") 
 
 userOptions.addEventListener("focus", ()=>{
     QuantityPerson.classList.add("QuantityPerson__show")
@@ -8,3 +9,9 @@ userOptions.addEventListener("focus", ()=>{
 userOptions.addEventListener("blur", ()=>{
     QuantityPerson.classList.remove("QuantityPerson__show")
 })
+
+for (let i = 0; i < optionItem.length; i++) {
+    optionItem[i].addEventListener("click", ()=>{
+        optionItem[i].classList.toggle("choose__option")
+    })
+}
